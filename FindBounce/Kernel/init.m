1e17154f0ed7563a50bcf1fd5adcb5ac1eb32830
PintarM <matevz.pintar@gmail.com>
2019-04-01 18:31:04 +0200

    Update PacletInfo.m and init.m to standard layout
    This enables loading the package from development directory
    with standard functions.

--- FindBounce/Kernel/init.m
+++ FindBounce/Kernel/init.m
@@ -1,3 +1,3 @@
 (* ::Package:: *)
 
-Get["FindBounce`"];
+Get["FindBounce`FindBounce`"];
--- FindBounce/PacletInfo.m
+++ FindBounce/PacletInfo.m
@@ -1,30 +1,22 @@
 (* ::Package:: *)
 
 (* Paclet Info File *)
+(* BuildNumber and Internal values are inserted during build procedure. *)
 Paclet[
-	Name -> "FindBounces",
-	Version -> "0.0.1",
+	Name -> "FindBounce",
+	Version -> "0.1.0",
 	WolframVersion -> "8.+",
 	Description -> "Computes tunneling transition with multiple scalar fields.",
 	Creator -> "Victor Guada",
-	Publisher -> "  ",
-	URL -> "https://github.com/",
 	Thumbnail->"FrontEnd/Icon.png",
 	Extensions -> {
 		{"Kernel",
 			Root -> ".",
-			Context ->{"FindBounces`"}
+			Context ->{"FindBounce`"}
 		},
 		{"Documentation",
 			Language -> "English",
-			MainPage -> "Guides/FindBounces"
-		}(*,
-		(* Metadata for PacletServer (https://paclets.github.io/PacletServer) *)
-		{"PacletServer",
-			"Tags" -> {"finite-elements","mesh","FEM"},
-			"Categories" -> {"FEM"},
-			"Description" -> "A package with utilities for creating and manipulating ElementMesh objects.",
-			"License" -> "MIT"
-		}*)
+			MainPage -> "Tutorials/Examples_FindBounce"
+		}
 	}
 ]
