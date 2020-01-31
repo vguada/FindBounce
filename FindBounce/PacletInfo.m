6d0ed208450b74c3d2b5ba7b1fbf212c6e71dbfe
Victor G <victor.guada@ijs.si>
2020-01-31 18:13:21 +0100

    Change version to 1-0-0

--- FindBounce/PacletInfo.m
+++ FindBounce/PacletInfo.m
@@ -1,10 +1,12 @@
+(* ::Package:: *)
+
 (* Paclet Info File *)
 
 (* Created on 2019/11/19 *)
 
 Paclet[
     Name -> "FindBounce",
-    Version -> "0.2.0",
+    Version -> "1.0.0",
     MathematicaVersion -> "10.0+",
     Description -> "Package for computing tunneling transition with multiple scalar fields.",
     Creator -> "Victor Guada, Miha Nemevsek and Matevz Pintar",
